c70940652ea9b3a0c774993859a75fc505653ef3
Elias Storme <elias.storme@gmail.com>
2017-03-01 17:30:40 +0100

    started creating basic inspectors and constructos

--- src/File.java
+++ src/File.java
@@ -1,4 +1,42 @@
 //@author jemoeder
-public class File {
+import java.util.regex.*;
 
+public class File {
+	private String name;
+	private boolean writable;
+	private int size;
+	
+	public File(String name, int size, boolean writable){
+		assert isValidSize(size);
+	}
+	
+	public File(String name){
+		this(name, 0, true);
+	}
+	
+	public int getSize(){
+		return size;
+	}
+	
+	private boolean isValidSize(int size){
+		return size >= 0 && size - 1 < Integer.MAX_VALUE;
+	}
+	
+	public void enlarge(int amount){
+		int newSize = amount + getSize();
+	}
+	
+	public void shorten(int amount){
+		
+	}
+	
+	public void setName(String name){
+		if (isValidName(name)) this.name = name;
+	}
+	
+	private boolean isValidName(String name){
+		if(name.length()==0) return false;
+		Pattern valid = Pattern.compile("[a-zA-Z]");
+		return valid.matcher(name).matches();
+	}
 }
