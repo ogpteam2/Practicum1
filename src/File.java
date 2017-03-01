7d894c78012caf664c3c315f6a460e6d982aa822
Elias Storme <elias.storme@gmail.com>
2017-03-01 20:14:10 +0100

    Started Date system, finished size and name system

--- src/File.java
+++ src/File.java
@@ -1,14 +1,19 @@
 //@author jemoeder
 import java.util.regex.*;
 import be.kuleuven.cs.som.annotate.*;
+import java.util.Date;
 
 public class File {
 	private String name;
 	private boolean writable;
 	private int size;
+	private Date creationDate;
 	
 	public File(String name, int size, boolean writable){
+		assert isValidName(name);
+		setName(name);
 		assert isValidSize(size);
+		
 	}
 	
 	public File(String name){
@@ -24,15 +29,21 @@ public class File {
 	}
 	
 	public void enlarge(int amount){
-		int newSize = amount + getSize();
+		setSize(getSize() + amount);
 	}
 	
 	public void shorten(int amount){
-		
+		setSize(getSize() - amount);
+	}
+	
+	private void setSize(int size){
+		assert isValidSize(size);
+		this.size = size;
 	}
 	
 	public void setName(String name){
-		if (isValidName(name)) this.name = name;
+		assert isValidName(name);
+		this.name = name;
 	}
 	
 	private boolean isValidName(String name){
