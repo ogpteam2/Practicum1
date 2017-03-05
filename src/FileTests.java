f84eb9a03ffa708e09252981b47d5fa289dd67fe
stormeuh <elias.storme@gmail.com>
2017-03-05 16:38:15 +0100

    Started JUnit test class

---
+++ src/FileTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileTests {
+	@BeforeClass public static void setUpBeforeClass() throws Exception {
+	}
+	
+	
+	
+}
