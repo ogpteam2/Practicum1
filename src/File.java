0cabfc5ef7d4a0854f2c1901ff55a400ee33f23e
stormeuh <elias.storme@gmail.com>
2017-03-05 18:48:40 +0100

    werk

--- src/File.java
+++ src/File.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * @version	1.0
  * @author Elias en Robbe
  *
+ *
  */
 
 
@@ -32,19 +33,25 @@ public class File {
 	 * 		  de grootte van het bestand
 	 * @param writable
 	 * 		  true als het bestand beschrijfbaar is 
+	 * @post als de naam niet geldig is wordt de naam: "New Text Document.txt"
 	 * 
 	 * 
 	 */
 	
 	public File(String name, int size, boolean writable){
-		assert isValidName(name); // moet totaal gebeuren niet nominaal
-		setName(name);
+		if (isValidName(name) == false){ setName("New Text Document.txt"); }
+		else { setName(name); }
 		assert isValidSize(size);
 		setSize(size);
+		this.writable = writable;
 		creationDate = new Date();
 		modificationDate = creationDate; // dit zorgt ervoor dat modificationDate gelijk is aan creationDate bij aanmaak bestand
 	}
-	
+	/**
+	 * Maakt een bestand aan met grootte nul en een gegeven naam die beschrijfbaar is.
+	 * @param name
+	 * 		  de gewenste naam van het bestand
+	 */
 	public File(String name){
 		this(name, 0, true);
 	}
@@ -56,34 +63,75 @@ public class File {
 		return size;
 	}
 	
+	/**
+	 * Controleert ofdat een grootte geldig is
+	 * @param size
+	 * 		  de te controleren grootte
+	 * @return true als het een geldige grootte is 
+	 * 		   false als het geen geldige grootte is
+	 */
+	@Raw
 	private boolean isValidSize(int size){
 		return size >= 0 && size - 1 < Integer.MAX_VALUE;
 	}
 	
+	
+	/**
+	 * 
+	 * @param amount
+	 * 		  de hoeveelheid waarmee de file moet vergroot worden (in bytes)
+	 */
 	public void enlarge(int amount){
 		setSize(getSize() + amount);
 	}
-	
+	/**
+	 * @pre de hoeveelheid mag niet groter zijn dan de grootte van het bestand
+	 * 		isValidSize(getSize() - amount);
+	 * @param amount
+	 * 		  de hoeveelheid waarmee de file moet verkleind worden (in bytes)
+	 */
 	public void shorten(int amount){
+		assert isValidSize(getSize() - amount);
 		setSize(getSize() - amount);
 	}
 	
-	
+	/**
+	 * verandert de grootte van het bestand
+	 * @pre de size moet geldig zijn
+	 *      isValidSize(size);
+	 * @param size
+	 * 		  de gewenste grootte van het bestand
+	 * 
+	 */
 	private void setSize(int size){
 		assert isValidSize(size);
 		this.size = size;
-		updateModificationTime(); // probleem want dit gaat niet hetzelfde zijn als creation date in het begin // opgelost
+		updateModificationTime(); 
 	}
-	
+	/**
+	 * verandert de naam van het bestand
+	 * @param name
+	 * 		  de nieuwe naam van het bestand
+	 * @post als de naam niet geldig is wordt de vorige naam behouden
+	 * 		 de aanpassingsdatum wordt dan ook niet aangepast
+	 */
 	public void setName(String name){
-		assert isValidName(name);
-		this.name = name;
-		updateModificationTime(); // probleem want dit gaat niet hetzelfde zijn als creation date in het begin // opgelost
+		if (isValidName(name) == false){ return;}
+		else 
+		{
+			this.name = name;
+			updateModificationTime(); 
+		}
+		
 	}
 	
+	/**
+	 * update de aanpassingsdatum
+	 */
 	private void updateModificationTime(){
 		modificationDate = new Date();
 	}
+	
 	/**
 	 * geeft de aanmaakdatum terug
 	 */
@@ -92,6 +140,7 @@ public class File {
 	public Date getCreationDate(){
 		return creationDate;
 	}
+	
 	/**
 	 * geeft de modificatieDatum terug
 	 * @post als de het bestand nog nooit aangepast is geeft het de aanmaakdatum terug
@@ -104,14 +153,30 @@ public class File {
 			return getCreationDate();
 		}
 	}
-	
+	/**
+	 * controleert de geldigheid van de naam
+	 * @param name
+	 * 		  de te controleren naam
+	 * @return true als het een geldige naam is
+	 *         false als het geen geldige naam is
+	 */
 	private boolean isValidName(String name){
 		if(name.length()==0) return false;
 		Pattern valid = Pattern.compile("[a-zA-Z]");
 		return valid.matcher(name).matches();
 	}
-	
-	// (StartA <= EndB) and (EndA >= StartB)
+	/**
+	 * de gebruiksperiode is het tijdsinterval tussen het
+	 *	  creatietijdstip en het tijdstip van laatste wijziging.
+	 *    Deze methode geeft weer of de gebruiksperiode van deze file en een andere file overlappen.
+	 *    
+	 *    Deze methode werkt door volgend stelling (StartA <= EndB) and (EndA >= StartB)
+	 *    Deze stelling is correct maar wordt hier niet bewezen.
+	 * @param other
+	 * 		  een andere file
+	 * @return true als ze een overlappend gebruikersperiode hebben
+	 * 		   false als ze geen overlappend gebruikersperiode hebben
+	 */
 	public boolean hasOverlappingUsePeriod(File other){
 		if (other == null){
 			return false;
@@ -125,13 +190,18 @@ public class File {
 		return false;
 	}
 	
-	
+	/**
+	 * Wijzigt de schrijfrechten van een bestand
+	 * @param writable
+	 * 		  een boolean die bepaalt ofdat het bestand schrijfbaar is of niet
+	 */
 	public void setWritability(boolean writable){
 		this.writable = writable;
 	}
 	
 	/**
 	 * geeft de schrijfbaarheid terug
+	 * @return boolean writable
 	 */
 	@Basic
 	public boolean getWritability(){
--- src/FileTests.java
+++ src/FileTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class FileTests {
 	@BeforeClass public static void setUpBeforeClass() throws Exception {
+		
 	}
 	
 	
